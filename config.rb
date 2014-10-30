73849e05a1e63dabee4cc34d0a728f6c18f43575
Arshad Chummun <arshad@chummun.com>
2014-10-30 01:28:28 -0500

    Multiple updates for initial site setup

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/drupal8notes/"
+http_images_path = "/drupal8notes/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/drupal8notes/fonts"
+css_dir = "public/drupal8notes/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
